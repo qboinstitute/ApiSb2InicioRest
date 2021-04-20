0512599fa4b023e6fb4faa25a4110c0850eef41a
Luis Angel Salvatierra Aquino <luisangel.salvat@gmail.com>
2021-04-19 22:14:27 -0500

    Peticiones GET

---
+++ src/main/java/com/qbo/controller/PersonaController.java
@@ -0,0 +1,49 @@
+package com.qbo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qbo.exception.ResourceNotFoundException;
+import com.qbo.model.Persona;
+import com.qbo.service.PersonaService;
+
+@RestController
+@RequestMapping("/apiqbo/v1")
+public class PersonaController {
+
+	@Autowired
+	private PersonaService personaService;
+	
+	@GetMapping("/persona")
+	public ResponseEntity<List<Persona>> listarPersonas(){
+		List<Persona> personas = new ArrayList<Persona>();
+		personaService.listarPersonas().forEach(personas::add);
+		if(personas.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(personas, HttpStatus.OK);
+	}
+	@GetMapping("/persona/{id}")
+	public ResponseEntity<Persona> obtenerPersonaPorId(
+			@PathVariable("id") Integer id
+			) throws ResourceNotFoundException{
+		Persona persona = personaService
+				.buscarPorId(id)
+				.orElseThrow(() -> new 
+						ResourceNotFoundException("Recurso con el id:"+
+				id +" no encontrado "));
+		return new ResponseEntity<>(persona, HttpStatus.OK);
+	}
+	
+	
+	
+	
+}
